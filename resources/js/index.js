1f920c071fed65a97f8adb40bdd7956426a52ae7
salsa-project <intenso524@gmail.com>
2019-05-17 05:31:30 +0100

    Add Animation To Mobile Navbar---- > DONE ; Fix Navbar In All Pages-----> DONE; +more styles(shadow,white_spaces....etc)------> DONE; Check if the window is resized----> DONE;

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,1 +1,19 @@
-const 
+const navBarToggle = document.getElementById('navBarToggle');
+const toggleLogo = document.getElementById('toggleLogo');
+const navBar = document.getElementById("navBar");
+
+//animation for the mobile navBar
+toggleLogo.addEventListener("click", function(){
+  navBar.style = `display: flex;`;
+  setTimeout(function(){navBar.style = `display: flex; animation-name: toUp; animation-duration: 2s;animation-fill-mode: forwards;`}, 2500)
+  
+})
+//check the size of the window is changed!
+window.addEventListener("resize", function(event) {
+  let w = document.body.clientWidth ;
+  if(w > 550){
+   navBar.style = `display: flex;`;
+  }else{
+    navBar.style = `display: none;`;
+  }
+})
